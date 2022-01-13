d3725b4441944cce54a0318be142c4d007e14f90
rizqyhbb <rizqyhbb@gmail.com>
2022-01-13 21:41:41 +0700

    change footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <div className=" font-extralight text-center text-xs text-opacity-20 py-5">
-      <p>By Rizqyhbb</p>
+      <p>Rizqyhbb</p>
     </div>
   )
 }
